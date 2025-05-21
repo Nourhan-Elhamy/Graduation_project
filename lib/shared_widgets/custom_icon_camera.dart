69d018b2f3593d0a1624d56cc5399c38cdbc146c
ezzat balah <ezzat.ahamed@icloud.com>
2025-05-21 20:14:22 +0300

    image

--- lib/shared_widgets/custom_icon_camera.dart
+++ lib/shared_widgets/custom_icon_camera.dart
@@ -1,12 +1,14 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:graduation_project/core/utils/app_colors.dart';
+import 'package:graduation_project/shared_widgets/image_screan.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:dio/dio.dart';
-
 import 'package:graduation_project/core/utils/services/Api_service.dart';
+import 'package:image/image.dart' as img;
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
 
 class CustomIconCamera extends StatefulWidget {
   final Function(String? imagePath)? onImageSelected;
@@ -32,45 +34,21 @@ class _CustomIconCameraState extends State<CustomIconCamera> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 5),
-          child: Container(
-            width: 40,
-            height: 40,
-            decoration: BoxDecoration(
-              color: AppColors.blue,
-              borderRadius: const BorderRadius.all(Radius.circular(8.0)),
-            ),
-            child: IconButton(
-              icon: const Icon(Icons.camera_alt),
-              color: AppColors.white,
-              onPressed: () async {
-                await _checkPermissions();
-                await _pickImageAndProcess();
-              },
-            ),
-          ),
-        ),
-        if (_pickedImagePath != null)
-          Image.file(
-            File(_pickedImagePath!),
-            width: 200,
-            height: 200,
-            fit: BoxFit.cover,
-          ),
-        if (_isLoading) const CircularProgressIndicator(),
-        if (_processResult != null && !_isLoading)
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Text(
-              _processResult!,
-              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-          ),
-      ],
+    return Container(
+      width: 40,
+      height: 40,
+      decoration: BoxDecoration(
+        color: AppColors.blue,
+        borderRadius: const BorderRadius.all(Radius.circular(8.0)),
+      ),
+      child: IconButton(
+        icon: const Icon(Icons.camera_alt),
+        color: AppColors.white,
+        onPressed: () async {
+          await _checkPermissions();
+          await _pickCompressAndProcess();
+        },
+      ),
     );
   }
 
@@ -87,26 +65,73 @@ class _CustomIconCameraState extends State<CustomIconCamera> {
     }
   }
 
-  Future<void> _pickImageAndProcess() async {
+  Future<void> _pickCompressAndProcess() async {
     final ImagePicker picker = ImagePicker();
     final XFile? pickedFile =
         await picker.pickImage(source: ImageSource.camera);
 
     if (pickedFile != null) {
       setState(() {
-        _pickedImagePath = pickedFile.path;
         _processResult = null;
         _isLoading = true;
       });
 
-      final result = await apiService.processImage(pickedFile.path);
+      // ضغط الصورة
+      final compressedPath = await compressImage(pickedFile.path, quality: 70);
 
-      if (!mounted) return;
+      if (compressedPath != null) {
+        setState(() {
+          _pickedImagePath = compressedPath;
+        });
 
-      setState(() {
-        _processResult = result;
-        _isLoading = false;
-      });
+        // ابعت الصورة المضغوطة للسيرفر
+        final result = await apiService.processImage(compressedPath);
+        print('Process result: $result');
+
+        if (!mounted) return;
+
+        setState(() {
+          _processResult = result;
+          _isLoading = false;
+        });
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ImageResultScreen(
+              imagePath: compressedPath,
+              resultText: result ?? 'No result',
+            ),
+          ),
+        );
+      } else {
+        setState(() {
+          _processResult = 'Failed to compress image';
+          _isLoading = false;
+        });
+      }
+    }
+  }
+
+  Future<String?> compressImage(String originalPath, {int quality = 80}) async {
+    try {
+      final originalFile = File(originalPath);
+      if (!await originalFile.exists()) return null;
+
+      final bytes = await originalFile.readAsBytes();
+      img.Image? image = img.decodeImage(bytes);
+      if (image == null) return null;
+
+      List<int> compressedBytes = img.encodeJpg(image, quality: quality);
+
+      final directory = await getTemporaryDirectory();
+      final targetPath = path.join(
+          directory.path, 'compressed_${path.basename(originalPath)}');
+      final compressedFile =
+          await File(targetPath).writeAsBytes(compressedBytes);
+
+      return compressedFile.path;
+    } catch (e) {
+      return null;
     }
   }
 }
